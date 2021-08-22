cf0b779de1f259ca20fbb4b10eaf21744cff34fe
Pratik282001 <54789620+Pratik282001@users.noreply.github.com>
2021-08-22 08:33:41 +0530

    added first commit

---
+++ day1/extrichk.cpp
@@ -0,0 +1,61 @@
+#include<bits/stdc++.h>
+#include<algorithm>
+using namespace std;
+#define gc getchar_unlocked
+#define ll long long
+#define PI 3.1415926535897932384626
+#define si(x) scanf("%d", &x)
+#define sl(x) scanf("%lld", &x)
+#define ss(s) scanf("%s", s)
+#define pi(x) printf("%d", x)
+#define pl(x) printf("%lld", x)
+#define ps(s) printf("%s", s)
+#define br printf("")
+#define fo(i, n) for(i=0;i<n;i++)
+#define Fo(i, k, n) for(i=k;k<n?i<n:i>n;k<n?i+=1:i-=1)
+#define deba(i, a, n) fo(i, n){cout << a[i] << " ";}
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define clr(x) memset(x, 0, sizeof(x))
+#define sortall(x) sort(all(x))
+#define tr(it, x) for(auto it = x.begin(); it != x.end(); it++)
+#define trr(it, x) for(auto it = x.rbegin(); it != x.rend(); it+)
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+mt19937_64 rang(chrono::high_resolution_clock::now().time_since_epoch().count());
+int rng(int lim) {
+    uniform_int_distribution<int> uid(0,lim-1);
+    return uid(rang);
+}
+const int mod = 1'000'000'007;
+const int N = 3e5;
+vi v[N];
+int a[N];
+
+
+void solution() {
+    int a,b,c;
+    cin>>a>>b>>c;
+    if(a==b && b==c) cout<<1;
+    else if((a==b && b!=c) && (a+b>c && b+c>a && c+a>b)) cout<<2;
+    else if((a!=b && b!=c && a!=c) && (a+b>c && b+c>a && c+a>b)) cout<<3;
+    else cout<<-1;
+
+}
+int main() {
+    srand(chrono::high_resolution_clock::now().time_since_epoch().count());
+    
+        solution();
+    
+    return 0;
+}
