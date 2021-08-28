903923faa1d94d1b3e4f934cb477444a433ad392
Pratik282001 <54789620+Pratik282001@users.noreply.github.com>
2021-08-28 15:18:49 +0530

    added somr

---
+++ day7/diffsum.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+#include<algorithm>
+using namespace std;
+int main() {
+    int n,m;
+    cin>>n>>m;
+    if(n>m) cout<<n-m<<endl;
+    else cout<<n+m<<endl;
+    return 0;
+}
