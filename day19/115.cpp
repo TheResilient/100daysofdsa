7f3b7386dc4f8b15b0c95d3c054cb1ebc7bfadf9
Pratik282001 <54789620+Pratik282001@users.noreply.github.com>
2021-09-09 15:47:21 +0530

    added

--- day19/115.cpp
+++ day19/115.cpp
@@ -42,30 +42,66 @@ const int N = 3e5;
 vi v[N];
 int a[N];
 
+vector< pair<int, int> > findSubArrays(int arr[], int n)
+{
+    // create an empty map
+    unordered_map<int, vector<int> > map;
+  
+    // create an empty vector of pairs to store
+    // subarray starting and ending index
+    vector <pair<int, int>> out;
+  
+    // Maintains sum of elements so far
+    int sum = 0;
+  
+    for (int i = 0; i < n; i++)
+    {
+        // add current element to sum
+        sum += arr[i];
+  
+        // if sum is 0, we found a subarray starting
+        // from index 0 and ending at index i
+        if (sum == 0)
+            out.push_back(make_pair(0, i));
+  
+        // If sum already exists in the map there exists
+        // at-least one subarray ending at index i with
+        // 0 sum
+        if (map.find(sum) != map.end())
+        {
+            // map[sum] stores starting index of all subarrays
+            vector<int> vc = map[sum];
+            for (auto it = vc.begin(); it != vc.end(); it++)
+                out.push_back(make_pair(*it + 1, i));
+        }
+  
+        // Important - no else
+        map[sum].push_back(i);
+    }
+  
+    // return output vector
+    return out;
+}
+
+void print(vector<pair<int, int>> out, int arr[])
+{
+    for (auto it = out.begin(); it != out.end(); it++){
+        for(int i=it->first; i<it->second; i++){
+            cout<<arr[i]<<" ";
+        }
+        cout<<endl;
+    }
+}
 
 void solution() {
     int n;
     cin>>n;
     int arr[n];
-    int sub[n][n];
-    int h=0;
-    for(int i=0; i<n; i++){
-        for(int j=i; j<n; j++){
-            int brr[n];
-            int l=0;
-            int sum=0;
-            for(int k=i; k<=j; k++){
-                sum+=arr[k];
-                brr[l]=arr[k];
-                l++;
-            }
-            if(sum==0){
-                sub[h]=brr;
-                h++;
-            }
-        }
-    }
-
+    for(int i=0; i<n; i++) cin>>arr[i];
+    vector<pair<int,int>>out = findSubArrays(arr, n);
+    if(out.size()==0) cout<<"no subarray with sum 0"<<endl;
+    else print(out, arr);
+    
 }
 int main() {
     srand(chrono::high_resolution_clock::now().time_since_epoch().count());
