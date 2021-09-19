59dd5554a8bee993b8c5f2de405aca71e37b455f
Pratik282001 <54789620+Pratik282001@users.noreply.github.com>
2021-09-19 15:16:31 +0530

    added

--- day28/69.cpp
+++ day28/69.cpp
@@ -42,31 +42,44 @@ const int N = 3e5;
 vi v[N];
 int a[N];
 
+int min(int x, int y, int z) { return min(min(x, y), z); }
+ 
+int editDist(string str1, string str2, int m, int n)
+{
+    // If first string is empty, the only option is to
+    // insert all characters of second string into first
+    if (m == 0)
+        return n;
+ 
+    // If second string is empty, the only option is to
+    // remove all characters of first string
+    if (n == 0)
+        return m;
+ 
+    // If last characters of two strings are same, nothing
+    // much to do. Ignore last characters and get count for
+    // remaining strings.
+    if (str1[m - 1] == str2[n - 1])
+        return editDist(str1, str2, m - 1, n - 1);
+ 
+    // If last characters are not same, consider all three
+    // operations on last character of first string,
+    // recursively compute minimum cost for all three
+    // operations and take minimum of three values.
+    return 1
+           + min(editDist(str1, str2, m, n - 1), // Insert
+                 editDist(str1, str2, m - 1, n), // Remove
+                 editDist(str1, str2, m - 1,
+                          n - 1) // Replace
+             );
+}
 
 void solution() {
-    string str;
-    cin>>str;
-    int n=str.length();
-    int count0 = 0, count1 = 0;
- 
-    int cnt = 0;
-    for (int i = 0; i < n; i++) {
-        if (str[i] == '0') {
-            count0++;
-        }
-        else {
-            count1++;
-        }
-        if (count0 == count1) {
-            cnt++;
-        }
-    }
- 
-    if (cnt == 0) {
-        cout<<-1<<endl;
-    }
- 
-    cout<<cnt<<endl;
+    string s1,s2;
+    cin>>s1>>s2;
+    // priority_queue<char> s;
+    cout<<editDist(s1, s2, s1.length(), s2.length())<<endl;
+
 
 }
 int main() {
