554f09c0f7bafdc3dd66bc0c09f71993822d5287
Pratik282001 <54789620+Pratik282001@users.noreply.github.com>
2021-09-25 19:37:37 +0530

    added

--- day33/74.cpp
+++ day33/74.cpp
@@ -1,58 +1,88 @@
-#include<bits/stdc++.h>
-#include<algorithm>
-using namespace std;
-#define gc getchar_unlocked
-#define ll long long
-#define PI 3.1415926535897932384626
-#define si(x) scanf("%d", &x)
-#define sl(x) scanf("%lld", &x)
-#define ss(s) scanf("%s", s)
-#define pi(x) printf("%d", x)
-#define pl(x) printf("%lld", x)
-#define ps(s) printf("%s", s)
-#define br printf("")
-#define fo(i, n) for(i=0;i<n;i++)
-#define Fo(i, k, n) for(i=k;k<n?i<n:i>n;k<n?i+=1:i-=1)
-#define deba(i, a, n) fo(i, n){cout << a[i] << " ";}
-#define pb push_back
-#define mp make_pair
-#define F first
-#define S second
-#define all(x) x.begin(), x.end()
-#define rall(x) x.rbegin(), x.rend()
-#define clr(x) memset(x, 0, sizeof(x))
-#define sortall(x) sort(all(x))
-#define tr(it, x) for(auto it = x.begin(); it != x.end(); it++)
-#define trr(it, x) for(auto it = x.rbegin(); it != x.rend(); it+)
-typedef pair<int, int> pii;
-typedef pair<ll, ll> pl;
-typedef vector<int> vi;
-typedef vector<ll> vl;
-typedef vector<pii> vpii;
-typedef vector<pl> vpl;
-typedef vector<vi> vvi;
-typedef vector<vl> vvl;
-mt19937_64 rang(chrono::high_resolution_clock::now().time_since_epoch().count());
-int rng(int lim) {
-    uniform_int_distribution<int> uid(0,lim-1);
-    return uid(rang);
+// C++ program for implementation of KMP pattern searching
+// algorithm
+#include <bits/stdc++.h>
+
+void computeLPSArray(char* pat, int M, int* lps);
+
+// Prints occurrences of txt[] in pat[]
+void KMPSearch(char* pat, char* txt)
+{
+	int M = strlen(pat);
+	int N = strlen(txt);
+
+	// create lps[] that will hold the longest prefix suffix
+	// values for pattern
+	int lps[M];
+
+	// Preprocess the pattern (calculate lps[] array)
+	computeLPSArray(pat, M, lps);
+
+	int i = 0; // index for txt[]
+	int j = 0; // index for pat[]
+	while (i < N) {
+		if (pat[j] == txt[i]) {
+			j++;
+			i++;
+		}
+
+		if (j == M) {
+			printf("Found pattern at index %d ", i - j);
+			j = lps[j - 1];
+		}
+
+		// mismatch after j matches
+		else if (i < N && pat[j] != txt[i]) {
+			// Do not match lps[0..lps[j-1]] characters,
+			// they will match anyway
+			if (j != 0)
+				j = lps[j - 1];
+			else
+				i = i + 1;
+		}
+	}
 }
-const int mod = 1'000'000'007;
-const int N = 3e5;
-vi v[N];
-int a[N];
 
+// Fills lps[] for given patttern pat[0..M-1]
+void computeLPSArray(char* pat, int M, int* lps)
+{
+	// length of the previous longest prefix suffix
+	int len = 0;
 
-void solution() {
+	lps[0] = 0; // lps[0] is always 0
 
+	// the loop calculates lps[i] for i = 1 to M-1
+	int i = 1;
+	while (i < M) {
+		if (pat[i] == pat[len]) {
+			len++;
+			lps[i] = len;
+			i++;
+		}
+		else // (pat[i] != pat[len])
+		{
+			// This is tricky. Consider the example.
+			// AAACAAAA and i = 7. The idea is similar
+			// to search step.
+			if (len != 0) {
+				len = lps[len - 1];
 
+				// Also, note that we do not increment
+				// i here
+			}
+			else // if (len == 0)
+			{
+				lps[i] = 0;
+				i++;
+			}
+		}
+	}
 }
-int main() {
-    srand(chrono::high_resolution_clock::now().time_since_epoch().count());
-    int t = 1;
-    si(t);
-    while(t--) {
-        solution();
-    }
-    return 0;
+
+// Driver program to test above function
+int main()
+{
+	char txt[] = "ABABDABACDABABCABAB";
+	char pat[] = "ABABCABAB";
+	KMPSearch(pat, txt);
+	return 0;
 }
