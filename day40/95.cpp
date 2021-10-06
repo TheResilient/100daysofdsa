1fff5ce33818a6e6c51bb2ceb09b2a123ce6ed63
Pratik282001 <54789620+Pratik282001@users.noreply.github.com>
2021-10-06 17:52:02 +0530

    added

--- day40/95.cpp
+++ day40/95.cpp
@@ -42,9 +42,69 @@ const int N = 3e5;
 vi v[N];
 int a[N];
 
+#define MAX_CHAR 26
+
+int runCustomerSimulation(int n, const string *seq)
+{
+    // seen[i] = 0, indicates that customer 'i' is not in cafe
+    // seen[1] = 1, indicates that customer 'i' is in cafe but
+    //             computer is not assigned yet.
+    // seen[2] = 2, indicates that customer 'i' is in cafe and
+    //             has occupied a computer.
+    char seen[MAX_CHAR] = {0};
+ 
+    // Initialize result which is number of customers who could
+    // not get any computer.
+    int res = 0;
+ 
+    int occupied = 0; // To keep track of occupied computers
+ 
+    // Traverse the input sequence
+    for (int i=0; seq[i]; i++)
+    {
+        // Find index of current character in seen[0...25]
+        int ind = seq[i] - 'A';
+ 
+        // If First occurrence of 'seq[i]'
+        if (seen[ind] == 0)
+        {
+            // set the current character as seen
+            seen[ind] = 1;
+ 
+            // If number of occupied computers is less than
+            // n, then assign a computer to new customer
+            if (occupied < n)
+            {
+                occupied++;
+ 
+                // Set the current character as occupying a computer
+                seen[ind] = 2;
+            }
+ 
+            // Else this customer cannot get a computer,
+            // increment result
+            else
+                res++;
+        }
+ 
+        // If this is second occurrence of 'seq[i]'
+        else
+        {
+        // Decrement occupied only if this customer
+        // was using a computer
+        if (seen[ind] == 2)
+            occupied--;
+        seen[ind] = 0;
+        }
+    }
+    return res;
+}
 
 void solution() {
-
+    string s;
+    int n;
+    cin>>n>>s;
+    cout<<runCustomerSimulation(n,s)<<endl;
 
 }
 int main() {
