af4a423a35c894f44223adf12db81f4d504b2c0f
Pratik282001 <54789620+Pratik282001@users.noreply.github.com>
2021-10-11 18:26:05 +0530

    added

---
+++ day43/125.cpp
@@ -0,0 +1,111 @@
+#include<bits/stdc++.h>
+#include<algorithm>
+using namespace std;
+#define gc getchar_unlocked
+#define ll long long
+#define PI 3.1415926535897932384626
+#define si(x) scanf("%d", &x)
+#define sl(x) scanf("%lld", &x)
+#define ss(s) scanf("%s", s)
+#define pi(x) printf("%d", x)
+#define pl(x) printf("%lld", x)
+#define ps(s) printf("%s", s)
+#define br printf("")
+#define fo(i, n) for(i=0;i<n;i++)
+#define Fo(i, k, n) for(i=k;k<n?i<n:i>n;k<n?i+=1:i-=1)
+#define deba(i, a, n) fo(i, n){cout << a[i] << " ";}
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define clr(x) memset(x, 0, sizeof(x))
+#define sortall(x) sort(all(x))
+#define tr(it, x) for(auto it = x.begin(); it != x.end(); it++)
+#define trr(it, x) for(auto it = x.rbegin(); it != x.rend(); it+)
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+mt19937_64 rang(chrono::high_resolution_clock::now().time_since_epoch().count());
+int rng(int lim) {
+    uniform_int_distribution<int> uid(0,lim-1);
+    return uid(rang);
+}
+const int mod = 1'000'000'007;
+const int N = 3e5;
+vi v[N];
+int a[N];
+
+void combinationUtil(int arr[], int n, int r,
+                     int index, int data[], int i);
+  
+// The main function that prints all combinations of
+// size r in arr[] of size n. This function mainly
+// uses combinationUtil()
+void printCombination(int arr[], int n, int r)
+{
+   
+    // A temporary array to store all combination
+    // one by one
+    int data[r];
+  
+    // Print all combination using temporary array 'data[]'
+    combinationUtil(arr, n, r, 0, data, 0);
+}
+  
+/* arr[]  ---> Input Array
+   n      ---> Size of input array
+   r      ---> Size of a combination to be printed
+   index  ---> Current index in data[]
+   data[] ---> Temporary array to store current combination
+   i      ---> index of current element in arr[]     */
+int * combinationUtil(int arr[], int n, int r, int index,
+                     int data[], int i)
+{
+    // Current combination is ready, print it
+    if (index == r) {
+        int returna[r];
+        for (int j = 0; j < r; j++)
+            returna[j]=data[j];
+        
+        return returna;
+    }
+  
+    // When no more elements are there to put in data[]
+    if (i >= n)
+        return;
+  
+    // current is included, put next at next location
+    data[index] = arr[i];
+    combinationUtil(arr, n, r, index + 1, data, i + 1);
+  
+    // current is excluded, replace it with next
+    // (Note that i+1 is passed, but index is not
+    // changed)
+    combinationUtil(arr, n, r, index, data, i + 1);
+}
+
+void solution() {
+    int n,c;
+    cin>>n>>c;
+    int arr[n];
+    for(int i=0; i<n; i++) cin>>arr[i];
+    sort(arr, arr+n);
+
+
+}
+int main() {
+    srand(chrono::high_resolution_clock::now().time_since_epoch().count());
+    int t = 1;
+    si(t);
+    while(t--) {
+        solution();
+    }
+    return 0;
+}
