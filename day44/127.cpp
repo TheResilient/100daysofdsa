02c1969addc99c2f4a770c9b525ca2209e011fc3
Pratik282001 <54789620+Pratik282001@users.noreply.github.com>
2021-10-12 18:42:23 +0530

    added

--- day44/127.cpp
+++ day44/127.cpp
@@ -1,1 +1,87 @@
-git st
+//incomplete
+
+#include<bits/stdc++.h>
+#include<algorithm>
+using namespace std;
+#define gc getchar_unlocked
+#define ll long long
+#define PI 3.1415926535897932384626
+#define si(x) scanf("%d", &x)
+#define sl(x) scanf("%lld", &x)
+#define ss(s) scanf("%s", s)
+#define pi(x) printf("%d", x)
+#define pl(x) printf("%lld", x)
+#define ps(s) printf("%s", s)
+#define br printf("")
+#define fo(i, n) for(i=0;i<n;i++)
+#define Fo(i, k, n) for(i=k;k<n?i<n:i>n;k<n?i+=1:i-=1)
+#define deba(i, a, n) fo(i, n){cout << a[i] << " ";}
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define clr(x) memset(x, 0, sizeof(x))
+#define sortall(x) sort(all(x))
+#define tr(it, x) for(auto it = x.begin(); it != x.end(); it++)
+#define trr(it, x) for(auto it = x.rbegin(); it != x.rend(); it+)
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+mt19937_64 rang(chrono::high_resolution_clock::now().time_since_epoch().count());
+int rng(int lim) {
+    uniform_int_distribution<int> uid(0,lim-1);
+    return uid(rang);
+}
+const int mod = 1'000'000'007;
+const int N = 3e5;
+vi v[N];
+int a[N];
+
+int findmaximumprofit(int arr[], int n){
+    if(n==1) return arr[0];
+
+}
+
+int binarySearch(Job jobs[], int index)
+{
+    // Initialize 'lo' and 'hi' for Binary Search
+    int lo = 0, hi = index - 1;
+  
+    // Perform binary Search iteratively
+    while (lo <= hi)
+    {
+        int mid = (lo + hi) / 2;
+        if (jobs[mid].finish <= jobs[index].start)
+        {
+            if (jobs[mid + 1].finish <= jobs[index].start)
+                lo = mid + 1;
+            else
+                return mid;
+        }
+        else
+            hi = mid - 1;
+    }
+  
+    return -1;
+}
+
+void solution() {
+
+
+}
+int main() {
+    srand(chrono::high_resolution_clock::now().time_since_epoch().count());
+    int t = 1;
+    si(t);
+    while(t--) {
+        solution();
+    }
+    return 0;
+}
