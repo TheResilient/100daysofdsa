e2ca2dda07ac3bfdc0202c72e4e5b948292720c1
Pratik282001 <54789620+Pratik282001@users.noreply.github.com>
2021-11-03 18:57:47 +0530

    added

--- day53/gym.py
+++ day53/gym.py
@@ -1,3 +1,27 @@
 # you can play as many turns as you want. In one turn, you choose two different blocks that share the same color and swap them.
 
-def swap()
+# def swap(dictionary):
+#     sort
+
+
+
+def sort_dictionary(dictionary):
+    sorted_dictionary = {}
+    for key in sorted(dictionary.keys()):
+        sorted_dictionary[key] = dictionary[key]
+    return sorted_dictionary
+
+def compare_dictionary(dictionary1, dictionary2):
+    if(dictionary1.keys() == dictionary2.keys()): return True
+    else: return False
+    
+
+if __name__ == '__main__':
+    n,colors = [int(x) for x in input().split()]
+    tdict = {}
+    for i in range(n):
+        number, color = [int(x) for x in input().split()]
+        tdict[number] = color
+    print(sort_dictionary(tdict))
+    # print(compare_values(tdict, sort_dictionary(tdict)))
+    print(compare_dictionary(tdict, sort_dictionary(tdict)))
