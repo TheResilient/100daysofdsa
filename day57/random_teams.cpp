04f99a0b63d647c314458fb36098a5edc759e44b
Pratik282001 <54789620+Pratik282001@users.noreply.github.com>
2021-12-01 17:50:15 +0530

    added

--- day57/random_teams.cpp
+++ day57/random_teams.cpp
@@ -1,3 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+//n participants of the competition were split into m teams in some manner so that each team has at least one participant. After the competition each pair of participants from the same team became friends.
+
+int minimum(int n, int m){
+    return (n/m + 1)*(n%m)*(n/m)/2 + (m-n%m)*(n/m)*(n/m-1)/2;
+}
+
+int maximum(int n, int m){
+    return (n-(m-1))*(n-(m-1)-1)/2;
+}
+
+int main(){
+    int n, m;
+    cin >> n >> m;
+    cout << minimum(n, m) << " " << maximum(n, m) << endl;
+    return 0;
+}
+
