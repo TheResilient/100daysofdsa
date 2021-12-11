691def2a59b10e9dbacca092b1d404ad2433bf16
Pratik282001 <54789620+Pratik282001@users.noreply.github.com>
2021-12-11 12:25:56 +0530

    added

--- day60/interesting_drink.cpp
+++ day60/interesting_drink.cpp
@@ -3,21 +3,43 @@
 using namespace std;
 
 
+
 void solve(){
-    int n;
-    cin >> n;
-    vector<int> a(n);
-    for(int i = 0; i < n; i++){
-        cin >> a[i];
+    // int n;
+    // cin >> n;
+    // int a[n];
+    // for(int i = 0; i < n; i++){
+    //     cin >> a[i];
+    // }
+    // int m;
+    // sort(a, a+n);
+    // cin >> m;
+    // for(int i = 0; i < m; i++){
+    //     int x;
+    //     cin >> x;
+    //     cout<<binary_search(a, x, 0, n-1)+1<<endl;
+    // }
+
+    int m,n,i,j,k,ans;
+    cin>>n;
+    int a[n],p;
+    int cnt=0;
+    for(i=0; i<n; i++)
+    {
+        cin>>a[i];
     }
-    int m;
-    sort(a.begin(), a.end());
-    cin >> m;
-    for(int i = 0; i < m; i++){
-        int x;
-        cin >> x;
-        
-        auto itr = find(a, a + n, x);
-        }
+    cin>>m;
+    sort(a,a+n);
+    while(m--)
+    {
+        cin>>k;
+        ans=upper_bound(a,a+n, k)-a;
+        cout<<ans<<endl;
     }
+    
+}
+
+int main(){
+    solve();
+    return 0;
 }
