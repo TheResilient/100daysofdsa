ed41b41b2d1ea5682b9803e1ba9d45b27b694f54
Pratik282001 <54789620+Pratik282001@users.noreply.github.com>
2021-12-26 10:09:27 +0530

    added

---
+++ day65/present_from_lena.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// pattern printing 
+//           0
+//         0 1 0
+//       0 1 2 1 0
+//     0 1 2 3 2 1 0
+//   0 1 2 3 4 3 2 1 0
+// 0 1 2 3 4 5 4 3 2 1 0
+//   0 1 2 3 4 3 2 1 0
+//     0 1 2 3 2 1 0
+//       0 1 2 1 0
+//         0 1 0
+//           0
+void pattern(int n){
+    int l=n;
+    for(int k=0; k<=n; k++){
+        for(int i=l; i>0; i--){
+            cout<<"  ";
+        }
+        // cout<<"0"<<endl;
+        for(int i=0; i<k; i++){
+            cout<<i<<" ";
+        }
+        for(int i=k; i>=0; i--){
+            cout<<i<<" ";
+        }
+        cout<<endl;
+        l--;
+    }
+    int l1=0;
+    for(int k=n; k>0; k--){
+        cout<<"  ";
+        // cout<<"0"<<endl;
+        int z;
+        for(int i=0; i<k; i++){
+            cout<<i<<" ";
+            z=i;
+        }
+        for(int i=z-1; i>=0; i--){
+            cout<<i<<" ";
+        }
+        
+        cout<<endl;
+        l1++;
+        for(int i=0; i<l1; i++){
+            cout<<"  ";
+        }
+    }
+}
+
+
+int main(){
+    int n;
+    cin>>n;
+    pattern(n);
+}
