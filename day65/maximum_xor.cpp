500b6fa02c9b4164b6ef9cd1c067988878d9fa9c
Pratik282001 <54789620+Pratik282001@users.noreply.github.com>
2021-12-27 16:12:27 +0530

    added

---
+++ day65/maximum_xor.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+long long array_xor(vector<long long>arr, int n){
+    long long xorx =0;
+    for(int i=0; i<n; i++){
+        xorx = xorx^arr[i];
+    }
+    long long ma = pow(2,32)-1;
+    long long k = ma^xorx;
+    return k;
+
+}
+
+int main(){
+    int n;
+    cin>>n;
+    vector<long long>arr;
+    for(int i=0; i<n; i++){
+        long long x;
+        cin>>x;
+        arr.push_back(x);
+    }
+
+    sort(arr.begin(), arr.end());
+    while(n>0){
+        cout<<array_xor(arr, n)<<endl;
+        arr.pop_back();
+        n--;
+    }
+}
