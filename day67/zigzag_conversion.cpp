fb431d0dd9d69d6c53915d759973d329e0b8c4c7
Pratik282001 <54789620+Pratik282001@users.noreply.github.com>
2022-01-05 12:09:15 +0530

    added

---
+++ day67/zigzag_conversion.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string convert(string s, int numRows){
+    if (numRows == 1) return s;
+    string ans = "";
+    int n = s.size();
+    int cycleLen = 2 * numRows - 2;
+    for (int i = 0; i < numRows; i++) {
+        for (int j = 0; j + i < n; j += cycleLen) {
+            ans += s[j + i];
+            if (i != 0 && i != numRows - 1 && j + cycleLen - i < n) {
+                ans += s[j + cycleLen - i];
+            }
+        }
+    }
+    return ans;
+}
+
+int main() {
+    string s = "PAYPALISHIRING";
+    int numRows = 3;
+    cout << convert(s, numRows) << endl;
+    return 0;
+}
