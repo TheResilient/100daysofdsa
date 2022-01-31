9c78a1ac6724fbe1fe4022101cd7e1dfa36e2119
Pratik282001 <54789620+Pratik282001@users.noreply.github.com>
2022-01-31 17:09:20 +0530

    added

--- day76/coin_change.cpp
+++ day76/coin_change.cpp
@@ -1,21 +1,44 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int coinChange(vector<int>& coins, int amount){
-    int dp[amount+1];
-    dp[0] = 0;
+// int coinChange(vector<int>& coins, int amount){
+//     int dp[amount+1];
+//     dp[0] = 0;
     
-    for(int i=1; i<=amount; i++){
-        dp[i]=INT_MAX;
-        for(int j=0; j<coins.size(); i++){
-            if(coins[j]<=i){
-                dp[i] = min(dp[i], dp[i-coins[j]]+1);
-            }
+//     for(int i=1; i<=amount; i++){
+//         dp[i]=INT_MAX-1;
+//         for(int j=0; j<coins.size(); i++){
+//             if(coins[j]<=i){
+//                 dp[i] = min(dp[i], dp[i-coins[j]]+1);
+//             }
+//         }
+//     }
+//     // return dp[amount];
+//     return dp[amount] == INT_MAX ? -1 : dp[amount];
+// }
+
+int coinChange(vector<int> &coins, int n)
+{
+    // creating the base dp array, with first value set to 0
+    int dp[++n];
+    dp[0] = 0;
+    // more convenient to have the coins sorted
+    sort(begin(coins), end(coins));
+    // populating our dp array
+    for (int i = 1; i < n; i++)
+    {
+        // setting dp[0] base value to 1, 0 for all the rest
+        dp[i] = INT_MAX;
+        for (int c : coins)
+        {
+            if (i - c < 0)
+                break;
+            // if it was a previously not reached cell, we do not add use it
+            if (dp[i - c] != INT_MAX)
+                dp[i] = min(dp[i], 1 + dp[i - c]);
         }
     }
-    int ans = dp[amount];
-    // return dp[amount];
-    return dp[amount] == INT_MAX ? -1 : dp[amount];
+    return dp[--n] == INT_MAX ? -1 : dp[n];
 }
 
 int main(){
