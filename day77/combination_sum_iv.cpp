590e97ea9d7b302d217e47635f078e7100a8dc88
Pratik282001 <54789620+Pratik282001@users.noreply.github.com>
2022-02-01 09:47:36 +0530

    added' :

--- day77/combination_sum_iv.cpp
+++ day77/combination_sum_iv.cpp
@@ -1,15 +1,36 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int combinationSum4(vector<int>& nums, int target){
-    vector<int> dp(target+1);
+// int combinationSum4(vector<int>& nums, int target){
+//     vector<int> dp(target+1);
+//     dp[0] = 1;
+//     for(int i=1; i<=target; i++){
+//         for(int n: nums){
+//             if(i>=n) dp[i]+=dp[i-n];
+//         }
+//     }
+//     return dp[target];
+// }
+
+int combinationSum4(vector<int> &nums, int target)
+{
+
+    vector<unsigned int> dp(target + 1);
+
     dp[0] = 1;
-    for(int i=1; i<=target; i++){
-        for(int n: nums){
-            if(i>=n) dp[i]+=dp[i-n];
+
+    sort(nums.begin(), nums.end());
+
+    for (int i = 1; i <= target; i++)
+    {
+        for (auto num : nums)
+        {
+            if (i < num)
+                break;
+            dp[i] += dp[i - num];
         }
     }
-    return dp[target];
+    return dp.back();
 }
 
 int main(){
