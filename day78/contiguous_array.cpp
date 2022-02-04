baecf68d92515a18c7d6fb5d96d73e8861fa82d5
Pratik282001 <54789620+Pratik282001@users.noreply.github.com>
2022-02-04 22:13:36 +0530

    added

---
+++ day78/contiguous_array.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int findMaxLength(vector<int> &nums)
+{
+    int maxlen = 0;
+    for (int i = 0; i < nums.size(); i++)
+    {
+        int zeroes = 0, ones = 0;
+        for (int j = i; j < nums.size(); j++)
+        {
+            if (nums[j] == 0)
+            {
+                zeroes++;
+            }
+            else
+            {
+                ones++;
+            }
+            if (zeroes == ones)
+            {
+                maxlen = max(maxlen, j - i + 1);
+            }
+        }
+    }
+    return maxlen;
+}
+
+int main(){
+    vector<int> nums = {0,1};
+    cout << findMaxLength(nums);
+    return 0;
+}
