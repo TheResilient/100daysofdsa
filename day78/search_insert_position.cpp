fd6687980caa2ca685b08a37b5e6f5698104d013
Pratik282001 <54789620+Pratik282001@users.noreply.github.com>
2022-02-10 18:43:46 +0530

    added

---
+++ day78/search_insert_position.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int serachInsert(vector<int>& nums, int target){
+    if(target<nums[0]) return 1;
+    else if(target>nums[nums.size()-1]) return nums.size();
+    for(int i=0; i<nums.size()-1; i++){
+        if(target>=nums[i] and target<=nums[i+1]){
+            return i+1;
+        }
+    }
+    return -1;
+}
+
+int main(){
+    vector<int> nums = {1,3,5,6};
+    int val=7;
+    cout<<serachInsert(nums, val);
+    return 0;
+}
