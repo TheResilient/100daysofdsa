f7875678f5c11b9e5cda9a40bce1d6771f930c42
Pratik282001 <54789620+Pratik282001@users.noreply.github.com>
2022-02-10 18:05:51 +0530

    added

--- day78/subarray_sum_equals_k.cpp
+++ day78/subarray_sum_equals_k.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int subarraySum(vector<int>&nums, int k){
+int subarraySum1(vector<int>&nums, int k){
     int count=0;
     for(int i=0; i<nums.size(); i++){
         int sum=0;
@@ -13,6 +13,21 @@ int subarraySum(vector<int>&nums, int k){
         }
     }
     return count;
+}
+
+int subarraySum(vector<int> &nums, int k)
+{
+    int cum = 0;       // cumulated sum
+    map<int, int> rec; // prefix sum recorder
+    int cnt = 0;       // number of found subarray
+    rec[0]++;          // to take into account those subarrays that begin with index 0
+    for (int i = 0; i < nums.size(); i++)
+    {
+        cum += nums[i];
+        cnt += rec[cum - k];
+        rec[cum]++;
+    }
+    return cnt;
 }
 
 int main(){
