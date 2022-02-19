2d4d8338d5f548b12a8c5d523154588fd67b4701
Pratik282001 <54789620+Pratik282001@users.noreply.github.com>
2022-02-19 16:23:39 +0530

    added

--- day80/minimize_deviation_in_array.cpp
+++ day80/minimize_deviation_in_array.cpp
@@ -1,6 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+
 int minimumDeviation(vector<int>& nums){
     
+}
+
+int main(){
+    vector<int> nums={2,10,8};
+    cout<<minimumDeviation(nums);
+    return 0;
 }
