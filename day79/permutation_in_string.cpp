65a666a02fe95fe65b4f8d1c264cebfe91e46b91
Pratik282001 <54789620+Pratik282001@users.noreply.github.com>
2022-02-19 14:40:55 +0530

    added

---
+++ day79/permutation_in_string.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool checkInclusion(string s1, string s2){
+    if(s1.size() > s2.size()) return false;
+    vector<int> v1(26, 0);
+    vector<int> v2(26, 0);
+    for(int i = 0; i < s1.size(); i++){
+        v1[s1[i] - 'a']++;
+        v2[s2[i] - 'a']++;
+    }
+    
+}
