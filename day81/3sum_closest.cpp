cfce34c17d1f3cf0829e76aab63c11376c8006d0
Pratik282001 <54789620+Pratik282001@users.noreply.github.com>
2022-03-04 21:43:34 +0530

    added

--- day81/3sum_closest.cpp
+++ day81/3sum_closest.cpp
@@ -17,12 +17,30 @@ int threeSumClosest(vector<int>& nums, int target) {
     return ans;
 }
 
-
+int threesumclosest(vector<int>& nums, int target){
+    sort(nums.begin(), nums.end());
+    int min=INT_MAX;
+    int ans;
+    int size = nums.size();
+     for (int i = 0; i <size-2; i++){
+        for(int j=i+1; j<size-1; j++){
+            for(int k=j+1; k<size; k++){
+                int sum = nums[i] + nums[j] + nums[k];
+                if(abs(sum-target)<min){
+                    min = abs(sum-target);
+                    ans = sum;
+                    // cout<<sum<<endl;
+                }
+            }
+        }
+     }
+     return ans;
+}
 
 int main(){
-    vector<int> nums={-1,2,1,-4};
+    vector<int> nums={0,0,0};
     int target=1;
-    int ans=threeSumClosest(nums, target);
+    int ans=threesumclosest(nums, target);
     cout<<ans<<endl;
     return 0;
 }
---
+++ day81/champagne_tower.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+double champagneTower(int poured, int query_row, int query_glass)
+{
+    double result[101][101] = {0.0};
+    result[0][0] = poured;
+    for (int i = 0; i < 100; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            if (result[i][j] >= 1)
+            {
+                result[i + 1][j] += (result[i][j] - 1) / 2.0;
+                result[i + 1][j + 1] += (result[i][j] - 1) / 2.0;
+                result[i][j] = 1;
+            }
+        }
+    }
+    return result[query_row][query_glass];
+}
+
+int main(){
+    int poured, query_row, query_glass;
+    cin>>poured>>query_row>>query_glass;
+    cout<<champagneTower(poured, query_row, query_glass);
+    return 0;
+}
