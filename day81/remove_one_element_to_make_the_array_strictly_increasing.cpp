03ced64a7cb9a610e21061daad9aa41f12facd0b
Pratik282001 <54789620+Pratik282001@users.noreply.github.com>
2022-03-08 10:35:48 +0530

    added

---
+++ day81/remove_one_element_to_make_the_array_strictly_increasing.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool canBeIncreasing(vector<int> &nums)
+{
+    int previous = nums[0];
+    bool used = false;
+    for (int i = 1; i < nums.size(); i++)
+    {
+        if (nums[i] <= previous)
+        { // it's not strictly increasing
+            if (used)
+                return false;
+            // we haven't used the element removal yet.
+            used = true;
+            if (i == 1 || nums[i] > nums[i - 2]) // we remove the element from i - 1 position because it's bigger, so we update previous.
+                previous = nums[i];
+            // else we remove current element and leave previous to it's existing value.
+        }
+        else
+            previous = nums[i];
+    }
+    return true;
+}
+
+int main(){
+    int n;
+    cin>>n;
+    vector<int> nums(n);
+    for(int i = 0; i < n; i++){
+        cin>>nums[i];
+    }
+    if(canBeIncreasing(nums)){
+        cout<<"YES";
+    }else{
+        cout<<"NO";
+    }
+    return 0;
+}
