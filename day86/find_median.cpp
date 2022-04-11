c661d1bfa1d45dd52ba342d7a801a148f3fb1f40
Pratik282001 <54789620+Pratik282001@users.noreply.github.com>
2022-04-11 22:20:42 +0530

    added

---
+++ day86/find_median.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class MedianFinder
+{
+    priority_queue<int> minheap;
+    priority_queue<int, vector<int>, greater<int>> maxheap;
+
+public:
+    MedianFinder()
+    {
+    }
+
+    void addNum(int num)
+    {
+        maxheap.push(num);
+        minheap.push(maxheap.top());
+        maxheap.pop();
+        if (minheap.size() > maxheap.size())
+        {
+            maxheap.push(minheap.top());
+            minheap.pop();
+        }
+    }
+
+    double findMedian()
+    {
+        if (maxheap.size() > minheap.size())
+            return maxheap.top();
+        return (maxheap.top() + minheap.top()) / 2.0;
+    }
+};
+
+int main()
+{
+}
