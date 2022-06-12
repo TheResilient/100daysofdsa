3e01f6ab84d3a4797a05d28f1c1512f38da417b1
Pratik282001 <54789620+Pratik282001@users.noreply.github.com>
2022-06-12 23:55:04 +0530

    added

---
+++ day91/maximum_unique_subarray.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int maximumUniqueSubarray(vector<int>& nums) {
+        int result = 0;
+        unordered_set<int> hset;
+        for (int i = 0, j = 0, win = 0; j < nums.size(); j++) {
+            while (hset.find(nums[j]) != hset.end()) {
+                hset.erase(nums[i]);
+                win -= nums[i];
+                i++;
+            }
+            hset.insert(nums[j]);
+            win += nums[j];
+            result = max(result, win);
+        }
+        return result;
+    }
+
+int main(){
+    vector<int> nums={1,3,2};
+    cout<<maximumUniqueSubarray(nums);
+    return 0;
+}
