683eb99f8d712af4a777856a441098dd3f4e4c81
Pratik282001 <54789620+Pratik282001@users.noreply.github.com>
2022-07-26 00:33:26 +0530

    added

---
+++ day92/avalara1.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+#include<sstream>
+using namespace std;
+
+int main(){
+    int n,m; cin>>n>>m;
+    long long int a=n;
+    for(int i=n; i<=m; i++){
+        a=a*i;
+        // cout<<n<<endl;
+    }
+    int d=0;
+    while(a%10==0){
+        a=a/10;
+        d++;
+    }
+    stringstream ss;
+    ss << a;
+    string s;
+    ss >> s;
+
+    stringstream ss1;
+    ss1 << d;
+    string s1;
+    ss1 >> s1;
+    cout<<s+" X "+"10^"+s1<<endl;
+    return 0;
+}
